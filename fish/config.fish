f12f7420c49120c49748b3e1626a4ceb9c850a93
venttarc <ahnafrahmanper@proton.me>
2025-04-16 23:11:48 +0600

    idk

--- fish/config.fish
+++ fish/config.fish
@@ -7,14 +7,14 @@ alias lla 'eza -la'
 alias ls eza
 alias ll 'eza -l'
 alias lt 'eza --tree'
-alias cfg='nvim ~/.config/fish/config.fish'
+alias cfg='hx ~/.config/fish/config.fish'
 alias swallow='pip3 install -U tidal-dl-ng gallery-dl'
 alias rename="perl-rename"
 alias c="clear"
 alias gogh='bash -c "$(curl -fsSL 'https://raw.githubusercontent.com/AvinashReddy3108/Gogh4Termux/master/install.sh')"'
 alias u='pacman -Syu'
-alias fart="pacman -Scc" 
-alias eat="pacman -S" 
+alias fart="yes | pacman -Scc"
+alias eat="pacman -S"
 alias shit='yes | pacman -Rsc'
 alias g='git'
 alias burp='pacman -Ss'
@@ -37,9 +37,9 @@ set -Ux FZF_DEFAULT_OPTS "
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 starship init fish | source
 enable_transience
- 
+
 #yazi
-export EDITOR=nvim
+export EDITOR=hx
 
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
